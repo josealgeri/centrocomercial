eaef19cc723afc94e23db2612245e52ccfc5baab
josealgeri <josealgeri@gmail.com>
2017-09-02 18:22:01 -0300

    ajustado repository

--- src/main/java/br/com/centrocomercial/repository/CategoriaRepository.java
+++ src/main/java/br/com/centrocomercial/repository/CategoriaRepository.java
@@ -6,9 +6,6 @@ import br.com.centrocomercial.model.Categoria;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 
-<<<<<<< HEAD
-=======
-	public List<Categoria> findByDescricaoContainingIgnoreCase(String descricao);
->>>>>>> 47586629275b411da8bd8abab667d80da2a46c4c
+
 
 }
