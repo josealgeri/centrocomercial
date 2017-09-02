2bdb2b28a8888f1a9ff992bb0ae98c926f10b021
josealgeri <josealgeri@gmail.com>
2017-09-02 17:55:24 -0300

    ajustes

--- src/main/java/br/com/centrocomercial/model/Categoria.java
+++ src/main/java/br/com/centrocomercial/model/Categoria.java
@@ -13,7 +13,7 @@ import lombok.Data;
 public class Categoria {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long idcategoria;
 
 	@Column(length = 45, nullable = false)
