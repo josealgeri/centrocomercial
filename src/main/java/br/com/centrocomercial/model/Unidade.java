bc7292f57e79027494ec5b5bee4678aee78a559f
jose.algeri <jose.algeri@ciss.com.br>
2017-09-04 15:00:50 -0300

    Ajuste da categoria. Agora não vai mais inserir um novo ao alterar o registro

--- src/main/java/br/com/centrocomercial/model/Unidade.java
+++ src/main/java/br/com/centrocomercial/model/Unidade.java
@@ -1,5 +1,7 @@
 package br.com.centrocomercial.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -9,7 +11,12 @@ import lombok.Data;
 @Entity
 
 @Data
-public class Unidade {
+public class Unidade implements Serializable {
+
+/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 /*	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
